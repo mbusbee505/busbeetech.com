156b8480a11a50fe7aac79525d2f9a6d14dfcfed
Michael Busbee <mbusbee505@gmail.com>
2025-09-28 23:26:51 -0400

    vault backup: 2025-09-28 23:26:51

--- source/quartz/components/Head.tsx
+++ source/quartz/components/Head.tsx
@@ -54,10 +54,10 @@ export default (() => {
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window,document,"clarity","script","ti4i9kql0e");
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ti4i9kql0e");
             `,
           }}
         />
